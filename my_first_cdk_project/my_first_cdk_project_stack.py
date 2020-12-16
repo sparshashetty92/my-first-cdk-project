06e88710c22fbca543f39c29fcf1086ad200e2c0
myusername <spashett@cisco.com>
2020-12-16 16:41:38 +0530

    initial commit

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import core
-
+from aws_cdk import aws_s3 as _s3
 
 class MyFirstCdkProjectStack(core.Stack):
 
@@ -7,3 +7,11 @@ class MyFirstCdkProjectStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="myfirstcdkproject0101",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
